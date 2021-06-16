f05db6a36af868c7f9318d02ace1c2907c51f34c
Daniel Dicker <P110094643@student.peterborough.ac.uk>
2021-06-16 16:55:14 +0100

    Project now working under /posts

---
+++ posty/resources/views/layouts/app.blade.php
@@ -0,0 +1,32 @@
+
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Posty</title>
+
+
+
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>Posty</title>
+
+</head>
+<body class="bg-grey-200">
+        <nav class="p-6 bg-red-500 flex justify-between mb-6">
+            <ul class="flex items-center">
+                <li>
+<ul>
+<li>
+<a  href=""> Home</a>
+</li>
+</ul>
+</nav>
+@yield('content')</body>
+</html>
